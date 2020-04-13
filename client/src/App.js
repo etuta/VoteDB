5b6b16181b5725e6f50e191679c31a1d1af44ff6
Tuta <etuta@middlebury.edu>
2020-04-13 12:27:11 -0400

    Hook issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,93 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect, useRef } from "react";
+// import Accordion from 'react-bootstrap/Accordion'
+// import Card from 'react-bootstrap/Card'
+import Popover from "react-bootstrap/Popover";
+import Overlay from "react-bootstrap/Overlay";
+import Tooltip from "react-bootstrap/Tooltip";
+//import logo from "./logo.svg";
 import "./App.css";
+import styled from "styled-components";
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { MDBCol, MDBInput } from "mdbreact";
+const Button = styled.button``;
 
 const App = () => {
+  const [target, setTarget] = useState(null);
+  const [dropdownOpen, setOpen] = useState(false);
+  const toggle = () => setOpen(!dropdownOpen);
+  const [show, setShow] = useState(false);
+  //  const target = useRef(null);
+  const ref = useRef(null);
+
+  const handleClick = event => {
+    setShow(!show);
+    setTarget(event.target);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to your CS312 Project</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Voter App</h1>
+        </header>
+
+        <p className="App-intro">Main page of the app</p>
+        <>
+          <Row>
+            <MDBCol md="6">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Search by Location"
+                aria-label="Search location"
+              />
+            </MDBCol>
+            <Button
+              onClick={handleClick}
+              margin-right="40px"
+              variant="Search on the Map"
+            >
+              Select on the Map
+            </Button>{" "}
+            &nbsp;
+            <Button
+              onClick={handleClick}
+              margin-left="400px"
+              variant="Search by Filters:"
+            >
+              {" "}
+              Search by Filters{" "}
+            </Button>{" "}
+            <Col></Col>
+          </Row>
+
+          <Row>
+            <Col sm={{ size: "auto", offset: 9 }}>
+              <ListGroup>
+                <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+                  <DropdownToggle caret>Party Affiliation</DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem>Democrat</DropdownItem>
+                    <DropdownItem>Republican</DropdownItem>
+                    <DropdownItem>Independent</DropdownItem>
+                  </DropdownMenu>
+                </ButtonDropdown>
+              </ListGroup>
+            </Col>
+          </Row>
+        </>
+      </div>
+    </Container>
   );
 };
 
