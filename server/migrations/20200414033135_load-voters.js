ba64293f0ae0006e1370e805764bcc5308f04f1b
Elva Osorio <osorioelva@gmail.com>
2020-04-27 18:01:18 -0400

    Sprint 2

--- server/migrations/20200414033135_load-voters.js
+++ server/migrations/20200414033135_load-voters.js
@@ -1,17 +1,19 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("Voters", table => {
-    table.increments("id").primary();
-    table.string("name").notNullable();
-    table.text("address").notNullable();
-    table.integer("times_contacted");
-    table.string("party").notNullable();
-    table.boolean("regstration_status").notNullable();
-    table.string("age_range").notNullable();
-    table.string("race").notNullable();
-    table.string("socioeconomic_status").notNullable();
+/* eslint-disable func-names */
+exports.up = function (knex) {
+  return knex.schema.createTable('Voters', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.text('address').notNullable();
+    table.integer('times_contacted');
+    table.string('party').notNullable();
+    table.boolean('regstration_status').notNullable();
+    table.string('age_range').notNullable();
+    table.string('race').notNullable();
+    table.string('socioeconomic_status').notNullable();
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("Voters");
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('Voters');
 };
+/* eslint-enable func-names */
