bef709ab1e6a348e30f6d20281a185ead6ba9ed4
William Kelley <williamkelley@Williams-MacBook-Air-560.local>
2020-05-10 17:32:25 -0400

    basic start up tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import EmailBar from "./components/EmailBar.js";
+import Location from "./components/Location.js";
+import MapBar from "./components/MapBar.js";
+import SearchBar from "./components/SearchBar.js";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import { mount } from "enzyme";
+import { act } from "react-dom/test-utils";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("App rendering tests", () => {
+  let app;
+
+  beforeEach(() => {
+    app = mount(<App />);
+  });
+
+  describe("App component initial content", () => {
+    test("Contains an Location component", () => {
+      expect(app).toContainExactlyOneMatchingElement(Location);
+    });
+    test("Contains an MapBar component", () => {
+      expect(app).toContainExactlyOneMatchingElement(MapBar);
+    });
+    test("Contains an SearchBar component", () => {
+      expect(app).toContainExactlyOneMatchingElement(SearchBar);
+    });
+    test("Does not display EmailBar on start up", () => {
+      expect(app).not.toContainExactlyOneMatchingElement(EmailBar);
+    });
+  });
+
+  describe("Filter tests", () => {
+    test();
+  });
 });
