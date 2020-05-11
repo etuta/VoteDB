74a9e8332827587eab69c283374cbdc50038d736
Elva Osorio <osorioelva@gmail.com>
2020-05-11 04:28:36 -0400

    DB w/ emails

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 const http = require('http');
 const { app } = require('./routes');
+
 console.log('Trying to listen');
 const server = http.createServer(app).listen(process.env.PORT || 3001);
 console.log('Listening on port %d', server.address().port); // eslint-disable-line no-console
